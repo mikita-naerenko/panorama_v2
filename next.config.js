e20576004e1fbe82563c7ec16394f3a46617b1e2
Mikita <tetropak555666@gmail.com>
2023-11-10 19:26:37 +0900

    ff

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   output: 'export',
-// }
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/panorama_v2', // Update this to the base path of your project
-  target: 'experimental-serverless-trace',
-};
+  output: 'export',
+}
+
+
 
 
 module.exports = nextConfig
