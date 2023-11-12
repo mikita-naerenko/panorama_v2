8d50fc44f7e69d116480937854c947ccd17cb17e
Mikita <tetropak555666@gmail.com>
2023-11-12 22:39:24 +0900

    d

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/panorama_v2',
+  // basePath: '/panorama_v2',
   trailingSlash: true,
-  // output: 'standalone',
+  
 }
 
 
